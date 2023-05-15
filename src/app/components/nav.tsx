e9880a125c0954e2466a5c2e9c31c5647b9c494a
Dilan <dilantienda90@gmail.com>
2023-05-15 08:50:57 -0600

    Creacion del proyecto examen1

---
+++ src/app/components/nav.tsx
@@ -0,0 +1,19 @@
+
+export default function navegacion(){
+    return(
+        <div className="contenedor d-flex bg-black navbar navbar-default">
+
+            <div className="perfil col-lg-6">
+                <img src="" alt="" />
+            </div>
+
+            <div className="col-lg-6 d-flex">
+                <a href="">Experiencia</a>
+                <a href="">Mas sobre Mi</a>
+                <a href="">Contacto</a>
+                <a href="">Proyectos</a>
+            </div>
+
+        </div>
+    )
+}
