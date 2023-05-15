2b424fc75c38c0dfe7088a3c14c5d11fbbe8e205
Dilan <dilantienda90@gmail.com>
2023-05-15 16:35:10 -0600

    Avance con la parte de habilidades

--- src/app/components/skills.tsx
+++ src/app/components/skills.tsx
@@ -1,22 +1,23 @@
 export default function skills(){
     return(
         <div className="skills container d-flex">
-            <div className="intro">
+            <div className="saludo intro col-lg-6">
                 <h1>My <span> Skills </span></h1>
-                <p>see fully what skills i have and performed, to develop the projects for you</p>
+                <p>See fully what skills i have and performed, to develop the projects for you</p>
+            
+                <a className="btnp btn-about" href="#">See Projects</a>
             </div>
-            <div className="list-skills">
+            <div className="list-skills col-lg-6">
                 <ol>
-                    <li>Html</li>
-                    <li>Css</li>
-                    <li>Boostrap</li>
-                    <li>React</li>
-                    <li>Git & github</li>
-                    <li>NextJs</li>
+                    <li><span>01.</span> Html 5</li>
+                    <li><span>02.</span> Css 3</li>
+                    <li><span>03.</span> Boostrap</li>
+                    <li><span>04.</span> React</li>
+                    <li><span>05.</span> Git & github</li>
+                    <li><span>06.</span> NextJs</li>
+                    <li><span>07.</span> JavaScript</li>
                 </ol>
             </div>
-
-
         </div>
     )
 }
